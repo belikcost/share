8de6397ad75c8bc4aae2ef9a81c2c628e4bd337f
belikcost <k.belik@worksolutions.ru>
2022-02-09 14:10:42 +0300

    Удалена директория с билдом

--- dist/enums.d.ts
+++
@@ -1,9 +0,0 @@
-declare enum SharedTypesEnum {
-    facebook = "fbButton",
-    twitter = "tw",
-    vkontakte = "vk",
-    whatsapp = "whatsapp",
-    telegram = "telegram",
-    copy = "copy"
-}
-export { SharedTypesEnum };
